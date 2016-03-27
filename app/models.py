6c59e7fbff8368a099ede27eab5d840f906ff4b0
Shivam Mevawala <shivmevawala@gmail.com>
2016-03-27 11:11:51 -0400

    added chaperones table in db and now webserver/chaperone works

--- app/models.py
+++ app/models.py
@@ -2,13 +2,29 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), index=True)
     age = db.Column(db.Integer)
     parent = db.Column(db.String(64))
     lunch = db.Column(db.Boolean, default=False)
     chaperone = db.Column(db.Boolean, default=False)
     chapName= db.Column(db.String(64), default = '')
     notes = db.Column(db.Text, default = '')
+    # email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
+
+class Chap(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    parent = db.Column(db.String(64))
+    kids = db.Column(db.Text, default = '')
     # email = db.Column(db.String(120), index=True, unique=True)
 
     def __repr__(self):
--- app/views.py
+++ app/views.py
@@ -5,13 +5,19 @@ from app import app, db, models
 def index():
     users = models.User.query.all() # fake user
     return render_template('index.html',
-                           title='Welcome',
-                           users=users)
+                           title='Welcome')
 
 @app.route('/list')
 def list():
-    users = models.User.query.all() # fake user
+    users = models.User.query.all()
     return render_template('list.html',
+                           title='List',
+                           users=users)
+
+@app.route('/chaperone')
+def chaperone():
+    users = models.Chap.query.all();
+    return render_template('chaperones.html',
                            title='List',
                            users=users)
 
@@ -20,12 +26,15 @@ def list():
 def login():
     error = None
     n=1
+    chapText = ""
     if request.method == 'POST':
         while(request.form['name'+str(n)]!=''):
             print(n)
             db.session.add(models.User(name=request.form['name'+str(n)], age = int(request.form['age'+str(n)]), parent = request.form['parent'], lunch = (request.form['lunch']!="false")
-            , chaperone = (request.form['chap']!="false"), chapName = request.form['chapn'], notes = request.form['notes']))
+            , chaperone = (request.form['chap']!="false"), chapName = request.form['chapn'], notes = request.form['notes'+str(n)]))
+            chapText+=request.form['name'+str(n)]+" Age:"+request.form['age'+str(n)]+", "
             n+=1
+        db.session.add(models.Chap(name  = request.form['chapn'], parent = request.form['parent'], kids = chapText))
         db.session.commit()
         flash('You registered')
         return redirect(url_for('list'))
